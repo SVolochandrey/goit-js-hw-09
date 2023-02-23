cc78486e2676433def6eb445ea4fba9017570f8e
SVolochandrey <113455948+SVolochandrey@users.noreply.github.com>
2023-02-23 17:51:43 +0000

    Deploying to gh-pages from @ SVolochandrey/goit-js-hw-09@b24714c211bf9660835a9cdc8ecf8bf42bf6cab7 🚀

---
+++ 01-color-switcher.c239ac78.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(function(l){d=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3),e.setAttribute("disabled",!0),r.removeAttribute("disabled")})),r.addEventListener("click",(function(t){clearInterval(d),e.removeAttribute("disabled"),r.setAttribute("disabled",!0)})),r.setAttribute("disabled",!0);
+//# sourceMappingURL=01-color-switcher.c239ac78.js.map
