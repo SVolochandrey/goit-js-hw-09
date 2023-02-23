b24714c211bf9660835a9cdc8ecf8bf42bf6cab7
SVolochandrey <volochan.andrey@gmail.com>
2023-02-23 19:51:07 +0200

    done 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timer = null;
+
+buttonStart.addEventListener('click', onChangeColorStart);
+buttonStop.addEventListener('click', onChangeColorStop);
+buttonStop.setAttribute('disabled', true);
+
+function onChangeColorStart(event){
+timer = setInterval(() => {
+let randomColor = getRandomHexColor();
+body.style.backgroundColor = randomColor;
+}, 1000)
+
+buttonStart.setAttribute('disabled', true);
+buttonStop.removeAttribute('disabled');
+}
+
+function onChangeColorStop(event){
+clearInterval(timer);
+
+buttonStart.removeAttribute('disabled');
+buttonStop.setAttribute('disabled', true);
+}
